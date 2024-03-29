d33b9c526e2e724718c9cfc2adb1f01b2bc6ea74
ki2183 <ki2183@naver.com>
2024-03-29 15:14:52 +0900

    0329_api_login

--- src/component/page/login/login.tsx
+++ src/component/page/login/login.tsx
@@ -1,39 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Page from '../Page'
 import axios from 'axios'
+
+type login_type = {
+    userId:string
+    password:string
+}
+
 function LoginPage(){
 
-    const [id,setId] = useState<string>('')
-    const write = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        e.preventDefault()
-        setId(e.target.value)
-    }
-    const submitButton = (e:React.MouseEvent<HTMLButtonElement>) =>{
-        e.preventDefault()
-        console.log('sss')
-        const dto = { userId: id } 
-        // axios.post('/memos/checkId',JSON.stringify(dto))
-        //     .then(res=>{alert(res)})
-        //     .catch(error => console.error('Error:', error) );
+    const [loginDto,setLoginDto] = useState<login_type>({
+        userId:"",
+        password:""
+    })
 
-        const test = () =>{
-            
-        axios.get('/memos/test', {
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          }).then(res => console.log(res.data))
-          .catch(err => console.log(err))
-        }
-        test()
+    const onChange_handler = (e:React.ChangeEvent<HTMLInputElement>,type:"userId"|"password")=>{
+        e.preventDefault()
+        setLoginDto(prev => ({...prev,[type]:e.target.value}))
     }
+
+    const submitButton = async (e:React.MouseEvent<HTMLButtonElement>) =>  {
+        e.preventDefault()
+        
+        const login_api = () =>{
+            axios.post("/memos/login",loginDto)
+            .then((res)=>{console.log(res.data)})
+            .catch((err) => {console.log(err)})
+        }
+        await login_api()
+    }
+
+    useEffect(()=>{
+        console.log(loginDto)
+    },[loginDto])
 
     return(
         <Page>
             <div className="w-full h-full flex justify-center items-center ">
                 <div>
                     <form className='h-auto w-auto flex flex-col'>
-                        <input value={id} onChange={write} placeholder='아이디'  className='bg-transparent' type="text" aria-labelledby="name-label" name="user_id"/>
+                        <input value={loginDto.userId} onChange={e=>onChange_handler(e,"userId")} placeholder='아이디'  className='bg-transparent' type="text" aria-labelledby="name-label" name="user_id"/>
+                        <input value={loginDto.password} onChange={e=>onChange_handler(e,"password")} placeholder='비밀번호' className='bg-transparent' type="password" aria-labelledby="name-label" name="user_id"/>
                         <button onClick={submitButton}>보내기</button>
                     </form>
                 </div>
