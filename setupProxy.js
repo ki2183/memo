2a05ce3019c5fffa1e8f822b2f8fafbc1a86de04
ki2183 <ki2183@naver.com>
2024-04-17 14:48:22 +0900

    finish

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app){
   app.use(
       createProxyMiddleware('/api', {
-          target: "http://3.17.75.159:8080",
+          target: "http://3.144.79.235:8080",
           changeOrigin: true
       })
   )
